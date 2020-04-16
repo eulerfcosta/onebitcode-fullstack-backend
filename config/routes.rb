a619755cdbf5eb4f73e3f553762f31bd377ac7a8
eulerfcosta <euler.dacosta@gmail.com>
2020-04-15 23:19:27 -0300

    c005- adicionado controllers e views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
-
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', { to: "favorites#create", on: :member }.merge(options)
+      delete '/favorite', { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories, only: [:index, :show]
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+        concerns :favoritable, favoritable_type: 'Album'
+      end
+      resources :search, only: :index
+      resources :favorites, only: :index
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
+
+    end
+  end
 end
