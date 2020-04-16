5e130ebd2155427c02f6c8d5acc8a562ee30ce53
eulerfcosta <euler.dacosta@gmail.com>
2020-04-16 01:08:01 -0300

    c010 - adição de rotas no react e adiconas das rotas do react ao rails

---
+++ app/javascript/screens/home/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const HomeScreen = () => {
+  return (
+    <Fragment>
+      <div>Home Screen</div>
+    </Fragment>
+
+  );
+}
+
+export default HomeScreen;
