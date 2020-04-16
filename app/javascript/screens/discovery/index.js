60e4e4fb77e7da9f2ab27ddb1f12c66c946176b7
eulerfcosta <euler.dacosta@gmail.com>
2020-04-16 04:10:36 -0300

    c012 - Implementção de navbar e navfooter

--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
 
+import NavbarFooter from '../../components/common/navbar_footer';
+
 const DiscoveryScreen = () => {
   return (
     <Fragment>
       <div>Discovery Screen</div>
+      <NavbarFooter/>
     </Fragment>
   );
 }
--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
 
+import NavbarFooter from '../../components/common/navbar_footer';
+
 const FavoritesScreen = () => {
   return (
     <Fragment>
       <div>Favorites Screen</div>
+      <NavbarFooter/>
     </Fragment>
 
   );
